ac9382485a33a34e27dbb8fa02ea22fc8f42d50a
CalebeSG <calebeguimaraes10@gmail.com>
2020-06-24 17:31:49 -0300

    finished header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-home">
+      <div className="page-home-content">
+
+        <header>
+          <div className="header-top">
+            <h1>Thursday, <span>10th</span></h1>
+            <p><span>12</span> Tasks</p>
+          </div>
+
+          <p>December</p>
+        </header>
+
+        <main>
+          <ul>
+            <li>Teste</li>
+            <li>teste 2</li>
+            <li>teste 3</li>
+            <li>sdfsdf</li>
+            <li>sdfsdfsdf</li>
+          </ul>
+        </main>
+      </div>
     </div>
   );
 }
